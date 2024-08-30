cdbb31f1b2f0fb77dd407ddb18bf1570c9d80178
soulixai vilasit <107106046+souvls@users.noreply.github.com>
2024-08-30 19:02:06 +0700

    soulixai

--- src/pages/api/seamless/balance.ts
+++ src/pages/api/seamless/balance.ts
@@ -11,31 +11,39 @@ export default async function handler(
             const { member_account, currency, sign } = req.body
             const result = await User.findOne({ Username: member_account });
             if (result) {
-                var amount = result.Amount;
-                if (currency === 'IDR2' || currency === 'KRW2' || currency === 'MMK2' || currency === 'VND2' || currency === 'LAK2' || currency === 'KHR2') {
-                    amount = amount / 1000
-                }
-                res.status(200).json(
-                    {
-                        "code": 0,
-                        "message": "",
-                        "balance": amount,
-                        "currency": currency,
-                        "sign": sign
+                if (currency === "IDR" || currency === "THB" || currency === 'IDR2' || currency === 'KRW2' || currency === 'MMK2' || currency === 'VND2' || currency === 'LAK2' || currency === 'KHR2') {
+                    var amount = result.Amount;
+                    if (currency === 'IDR2' || currency === 'KRW2' || currency === 'MMK2' || currency === 'VND2' || currency === 'LAK2' || currency === 'KHR2') {
+                        amount = amount / 1000
                     }
-                );
+                    res.status(200).json(
+                        {
+                            "code": 0,
+                            "message": "",
+                            "balance": amount,
+                            "currency": currency,
+                            "sign": sign
+                        }
+                    );
+                } else {
+                    res.status(400).json(
+                        {
+                            "code": 999,
+                            "message": "Expected return Invalid Currency",
+                        }
+                    );
+                }
             } else {
-                res.status(200).json(
+                res.status(400).json(
                     {
-                        "code": 1000,
+                        "code": 999,
                         "message": "",
                         "balance": 0
                     }
                 );
             }
         } catch (err) {
-            console.log(err);
-            res.status(200).json(
+            res.status(400).json(
                 {
                     "code": 999,
                     "message": "",
