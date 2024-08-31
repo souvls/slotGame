cb21ad59fe63e9e903f0f94b7856f5384e2adb45
soulixai vilasit <107106046+souvls@users.noreply.github.com>
2024-09-01 01:24:55 +0700

    sss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "infinity999.com",
   description: "web game slot",
-  icons:{
-    icon:['/favicon.ico?v=4'],
-    apple:['/apple-touch-icon.png?v=4'],
-    shortcut:['/apple-touch-icon.png']
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png']
   }
 };
 
@@ -24,9 +24,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
         {children}
+        <SpeedInsights />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
       </body>
-
     </html>
   );
 }
