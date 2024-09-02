83ddbb387fa21de7a094b5df421e84a8f761cdbd
soulixai vilasit <107106046+souvls@users.noreply.github.com>
2024-09-02 16:55:33 +0700

    deposit

--- src/pages/api/seamless/deposit.ts
+++ src/pages/api/seamless/deposit.ts
@@ -11,118 +11,53 @@ export default async function handler(
     if (req.method === 'POST') {
         try {
             const { member_account, currency, transactions, operator_code, request_time, sign } = req.body;
-            const originalSign = md5(operator_code + request_time + "deposit" + process.env.SECRET_KEY);
-
-            //check transaction
-            const duplicate = await Transaction.findOne({ id: transactions[0].id })
-            if (duplicate) {
-                res.status(200).json(
-                    {
-                        "code": 1003,
-                        "message": " Duplicate Transaction",
-                    }
-                );
-                return;
-            }
-            if (transactions[0].action !== 'SETTLED') {
-                res.status(200).json(
-                    {
-                        "code": 1004,
-                        "message": "Expected to Return Invalid Action",
-                        "before_balance": 0,
-                        "balance": 0
-                    }
-                );
-                return;
-            }
-            if (currency !== "IDR" && currency !== "THB" && currency !== 'IDR2' && currency !== 'KRW2' && currency !== 'MMK2' && currency !== 'VND2' && currency !== 'LAK2' && currency !== 'KHR2') {
-                res.status(200).json(
-                    {
-                        "code": 1004,
-                        "message": "Expected to Return Invalid Currency",
-                        "before_balance": 0,
-                        "balance": 0
-                    }
-                );
-                return
-            }
-            if (!member_account) {
-                res.status(200).json(
-                    {
-                        "code": 1000,
-                        "message": "Member not Exist",
-                        "before_balance": 0,
-                        "balance": 0
-                    }
-                );
-                return
-            }
-            if (sign !== originalSign) {
-                res.status(200).json(
-                    {
-                        "code": 1004,
-                        "message": "Invalid Sign",
-                    }
-                );
-                return
-            }
-
-            User.findOne({ Username: member_account })
-                .then((result: any) => {
-                    if (!result) {
-                        res.status(200).json(
-                            {
-                                "code": 1000,
-                                "message": "Member not Exist",
-                            }
-                        );
-                        return;
-                    }
-                    new Transaction({
-                        "id": transactions[0].id,
-                        "amount": transactions[0].amount,
-                        "bet_amount": transactions[0].bet_amount,
-                        "valid_bet_amount": transactions[0].valid_bet_amount,
-                        "prize_amount": transactions[0].prize_amount,
-                        "tip_amount": transactions[0].tip_amount,
-                        "action": transactions[0].action,
-                        "wager_code": transactions[0].wager_code,
-                        "wager_status": transactions[0].wager_status,
-                        "payload": transactions[0].payload,
-                        "settled_at": transactions[0].settled_at,
-                        "game_code": transactions[0].game_code
-                    }).save();
-                    User.findOneAndUpdate(
-                        { _id: result._id },
-                        { $inc: { Amount: parseInt(transactions[0].amount) } },
-                        { new: true }
-                    ).then((newBalance: any) => {
+            const user = await User.findOne({ Username: member_account })
+            if (user) {
+                const transaction = await Transaction.find();
+                //check duplicate
+                const duplicate = transaction.find((item: any) => item.id === transactions[0].id);
+                if (!duplicate) {
+                    const wager = transaction.find((item: any) => item.wager_code === transactions[0].wager_code);
+                    if (wager) {
+                        const updateuser = await User.findOneAndUpdate(
+                            { _id: user._id },
+                            { $inc: { Amount: parseInt(transactions[0].amount) } },
+                            { new: true }
+                        )
                         res.status(200).json(
                             {
                                 "code": 0,
                                 "message": "",
-                                "before_balance": result.Amount,
-                                "balance": newBalance.Amount
+                                "before_balance": user.Amount,
+                                "balance": updateuser.Amount
                             }
                         );
-                    }).catch((err: any) => {
-                        //console.log(err);
+                        new Transaction(transactions[0]).save();
+                    } else {
                         res.status(200).json(
                             {
-                                "code": 1000,
-                                "message": err,
+                                "code": 1006,
+                                "message": " Bet Not Exist",
                             }
                         );
-                    });
-                }).catch((err: any) => {
+                        return;
+                    }
+                } else {
                     res.status(200).json(
                         {
-                            "code": 1000,
-                            "message": err,
+                            "code": 1003,
+                            "message": " Duplicate Transaction",
                         }
                     );
-                })
-
+                }
+            } else {
+                res.status(200).json(
+                    {
+                        "code": 1000,
+                        "message": "Member not Exist",
+                    }
+                );
+            }
         } catch (err) {
             console.log(err);
             res.status(200).json(
