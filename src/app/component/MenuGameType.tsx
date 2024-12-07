770a4560df84e5c1bc5845097596e7427adbb669
soulixai vilasit <107106046+souvls@users.noreply.github.com>
2024-12-07 14:24:54 +0700

    ss

--- src/app/component/MenuGameType.tsx
+++ src/app/component/MenuGameType.tsx
@@ -12,6 +12,7 @@ import LiveCasino from './LiveCasino';
 import Poker from './Poker';
 import Fishing from './Fishing';
 import Fooball from './Football';
+import ShowGameItem from './ShowGameItem';
 const menu = [
     { title: "slot", icon: Slot_icon, label: "สล็อต" },
     { title: "casino", icon: Casino_icon, label: "คาสิโน" },
@@ -19,28 +20,50 @@ const menu = [
     { title: "fishing", icon: Fish_icon, label: "ยิงปลา" },
     { title: "football", icon: Football_icon, label: "ฟุตบอล" },
 ];
+interface Game {
+    game_code: string,
+    game_name: string,
+    game_type: string,
+    image_url: string,
+    product_code: number,
+    product_id: number,
+    status: string,
+    support_currency: string,
+}
 const MenuGameType = () => {
     const [selectGame, setSelectGame] = useState("slot");
-
-    useEffect(()=>{
+    const [gameHistory, setgGmeHistory] = useState<Game[]>();
+    useEffect(() => {
         const x = localStorage.getItem("game_type_active");
-        if(x){
+        const y = localStorage.getItem("game_history");
+        const gameHistory: Game[] = y ? JSON.parse(y) : [];
+        setgGmeHistory(gameHistory);
+        if (x) {
             setSelectGame(x);
         }
-    },[])
+    }, [])
     const handleSelect = (title: string) => {
         setSelectGame(title);
-        localStorage.setItem("game_type_active",title);
+        localStorage.setItem("game_type_active", title);
     }
     return (
         <>
-            <div className='sm:w-full md:w-[960px] lg:w-[1200px] mx-auto border-b '>
-                <div className='w-full lg:w-[50%] px-3 py-3 mx-auto flex justify-center items-center gap-8 '>
-                    {menu.map((item, index) => {
+            <div className='w-full lg:w-[50%] px-3 py-3 mx-auto flex justify-center items-center gap-8 '>
+                {menu.map((item, index) => {
+                    return (
+                        <div key={index} onClick={() => handleSelect(item.title)} className={`flex flex-col items-center duration-500 transform hover:scale-125 hover:text-yellow-300 font-bold `}>
+                            <Image className={`${item.title === selectGame ? 'w-[50px] lg:w-[70px]' : 'w-[20px] lg:w-[40px]'}   `} alt="icon" src={item.icon} />
+                            <h1 className={` text-[8px] lg:text-lg ${item.title === selectGame ? 'text-yellow-400' : 'text-yellow-200'}`}>{item.label}</h1>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className='sm:w-full md:w-[960px] lg:w-[1200px] mx-auto'>
+                <div className='flex justify-center items-center gap-2 lg:gap-5 mb-2'>
+                    {gameHistory && gameHistory.map((item, index) => {
                         return (
-                            <div key={index} onClick={() => handleSelect(item.title)} className={`flex flex-col items-center duration-500 transform hover:scale-125 hover:text-yellow-300 font-bold `}>
-                                <Image className={`${item.title === selectGame ? 'w-[50px] lg:w-[70px]' : 'w-[20px] lg:w-[40px]'}   `} alt="icon" src={item.icon} />
-                                <h1 className={` text-[8px] lg:text-lg ${item.title === selectGame ? 'text-yellow-400' : 'text-yellow-200'}`}>{item.label}</h1>
+                            <div key={index} className='p-2 w-20 h-24 bg-red-500 rounded-lg hover:bg-yellow-300'>
+                                <ShowGameItem product_name={""} game={item} />
                             </div>
                         )
                     })}
@@ -51,7 +74,6 @@ const MenuGameType = () => {
             {selectGame === "poker" && <Poker />}
             {selectGame === "fishing" && <Fishing />}
             {selectGame === "football" && <Fooball />}
-
 
         </>
     )
--- src/app/component/ShowGameItem.tsx
+++ src/app/component/ShowGameItem.tsx
@@ -4,8 +4,9 @@ import { useRouter } from 'next/navigation';
 
 import md5 from "md5";
 import Cookies from 'js-cookie';
-import router from "next/router";
 import Swal from "sweetalert2";
+import Spinner from "./Spinner";
+import axios from "axios";
 interface Game {
     game_code: string,
     game_name: string,
@@ -46,14 +47,20 @@ const ShowGameItem: React.FC<Props> = ({ product_name, game }) => {
         };
     }, []);
     const handdlePlay = async () => {
+        if (game.game_type === "FISHING") {
+            Swal.fire({
+                icon: "warning",
+                title: "<p>ຂໍອະໄພ</p>",
+                html: "<p>ເກມກຳລັງປັບປຸງ</p>"
+            })
+            return
+        }
         try {
             const cookie = Cookies.get("userdata");
             if (cookie) {
                 setLoading(true);
                 //const token = JSON.parse(cookie).token;
                 const ip = await fetch("https://api.ipify.org/?format=json").then((response) => response.json());
-                const myHeaders = new Headers();
-                myHeaders.append("Content-Type", "application/json");
                 const request_time = new Date().getTime();
 
                 const hash = md5(`${request_time}${process.env.NEXT_PUBLIC_SECRET_KEY}launchgame${process.env.NEXT_PUBLIC_OP_CODE}`);
@@ -65,45 +72,34 @@ const ShowGameItem: React.FC<Props> = ({ product_name, game }) => {
                     "game_code": game.game_code,
                     "product_code": game.product_code,
                     "game_type": game.game_type,
-                    "language_code": 0,
+                    "language_code": 3,
                     "ip": ip.ip,
                     "platform": "web",
                     "sign": hash,
                     "request_time": request_time,
                     "operator_lobby_url": "http://infinity999.com",
                 }
-                fetch(`${process.env.NEXT_PUBLIC_API_NAME}/api/operators/launch-game`, {
-                    method: "POST",
-                    headers: myHeaders,
-                    body: JSON.stringify(raw),
-                    redirect: "follow"
-                })
-                    .then((response) => response.json())
-                    .then((result) => {
-                        console.log(result)
-                        if (result.code === 200) {
-                            router.push(result.url)
-                        } else {
-                            Swal.fire({
-                                icon: "warning",
-                                title: "<p>ຂໍອະໄພ</p>",
-                                html: "<p>ເກມກຳລັງປັບປຸງ</p>"
-                            })
+                const res = await axios.post(`${process.env.NEXT_PUBLIC_API_NAME}/api/operators/launch-game`,
+                    JSON.stringify(raw),
+                    {
+                        headers: {
+                            'Content-Type': 'application/json'
                         }
+                    }
 
-                    }).catch((err) => {
-                        console.log(err);
+                )
+                if (res.data) {
+                    if (res.data.code === 200) {
+                        saveGameHistory();
+                        router.push(res.data.url)
+                    } else {
                         Swal.fire({
-                            title: "<p>ຕິດຕໍ່ເອເຢັ້ນ</p>",
-                            text: "020 98 399 064",
-                            icon: "error",
-                            background: '#000000',
-                            color: '#ffffff',
-                            showConfirmButton: false,
-                        }).then(() => {
-                            //window.location.reload();
-                        });
-                    }).finally(() => setLoading(false))
+                            icon: "warning",
+                            title: "<p>ຂໍອະໄພ</p>",
+                            html: "<p>ເກມກຳລັງປັບປຸງ</p>"
+                        })
+                    }
+                }
             } else {
                 Swal.fire({
                     title: "<p>ຕິດຕໍ່ເອເຢັ້ນ</p>",
@@ -119,28 +115,36 @@ const ShowGameItem: React.FC<Props> = ({ product_name, game }) => {
             console.log(err);
             setLoading(false);
             Swal.fire({
-                title: "<p>ຕິດຕໍ່ເອເຢັ້ນ</p>",
-                text: "02011223344",
-                icon: "error",
-                background: '#000000',
-                color: '#ffffff',
-                showConfirmButton: false,
-            });
+                icon: "warning",
+                title: "<p>ຂໍອະໄພ</p>",
+                html: "<p>ເກມກຳລັງປັບປຸງ</p>"
+            })
         }
+    }
+    const saveGameHistory = () => {
+        const temp: Game[] = [];
+        const gameHistory = localStorage.getItem("game_history");
+        const Games: Game[] = gameHistory ? JSON.parse(gameHistory) : [];
+        temp.push(game);
+        for (var i = 0; i < 4; i++) {
+            temp.push(Games[i]);
+        }
+        localStorage.setItem("game_history", JSON.stringify(temp));
     }
     return (
         <div ref={imgRef} onClick={handdlePlay} className='w-full'>
             {isVisible &&
                 <>
+                    {loading && <Spinner />}
                     <img
-                        src={game.image_url}
+                        src={game?.image_url}
                         // onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
                         //     const img = e.target as HTMLImageElement;
                         //     img.src = "/assets/icon/product/"+product_name+".png"
                         //     img.onerror = null; // Ngăn chặn vòng lặp vô hạn nếu hình ảnh thay thế cũng bị lỗi
 
                         // }
-                        alt={game.game_name}
+                        alt={game?.game_name}
                         loading="lazy"
                         className='w-[100%] rounded-xl overflow-hidden flex justify-center items-center hover:border-2 border-yellow-300'
                     />
@@ -153,7 +157,7 @@ const ShowGameItem: React.FC<Props> = ({ product_name, game }) => {
                 }}
                 className='w-full rounded-xl overflow-hidden flex justify-center items-center hover:border-2 border-yellow-300'
             /> */}
-                    <h1 className=' text-center text-white text-[8px]'>{game.game_name}</h1>
+                    <h1 className=' text-center text-white text-[8px]'>{game?.game_name}</h1>
                     {/* <p className=' text-center text-white'>{item.product_code}{item.game_code}</p>  */}
                 </>}
         </div>
