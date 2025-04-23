89c4bef51adda9511482e64a9aa6483bafa2827c
soulixai vilasit <107106046+souvls@users.noreply.github.com>
2025-04-24 04:28:35 +0700

    slot

--- src/app/component/GameSlot.tsx
+++ src/app/component/GameSlot.tsx
@@ -9,21 +9,21 @@ import products from '@/gamedata/slot/products.json'
 import pg_soft from '@/gamedata/slot/pg_soft.json'
 import playstar from '@/gamedata/slot/playstar.json'
 import pragmatic_play from '@/gamedata/slot/pragmatic_play.json'
-// import fachai from '@/gamedata/slot/fachai.json'
-// import jili_tcg from '@/gamedata/slot/jili_tcg.json'
-// import cq9 from '@/gamedata/slot/cq9.json';
-// import live_22 from '@/gamedata/slot/live_22.json'
-// import netent from '@/gamedata/slot/netent.json'
-// import no_limit_city from '@/gamedata/slot/no_limit_city.json'
-// import evoplay from '@/gamedata/slot/evoplay.json'
-// import jdb from '@/gamedata/slot/jdb.json'
-// import redtiger from '@/gamedata/slot/redtiger.json'
-// import booming_games from '@/gamedata/slot/booming_games.json'
-// import wow_gaming from '@/gamedata/slot/wow_gaming.json'
-// import hacksaw from '@/gamedata/slot/hacksaw.json'
-// import octoplay from '@/gamedata/slot/octoplay.json'
-// import rich88 from '@/gamedata/slot/rich88.json'
-// import smartsoft from '@/gamedata/slot/smartsoft.json'
+import fachai from '@/gamedata/slot/fachai.json'
+import jili_tcg from '@/gamedata/slot/jili_tcg.json'
+import cq9 from '@/gamedata/slot/cq9.json';
+import live_22 from '@/gamedata/slot/live_22.json'
+import netent from '@/gamedata/slot/netent.json'
+import no_limit_city from '@/gamedata/slot/no_limit_city.json'
+import evoplay from '@/gamedata/slot/evoplay.json'
+import jdb from '@/gamedata/slot/jdb.json'
+import redtiger from '@/gamedata/slot/redtiger.json'
+import booming_games from '@/gamedata/slot/booming_games.json'
+import wow_gaming from '@/gamedata/slot/wow_gaming.json'
+import hacksaw from '@/gamedata/slot/hacksaw.json'
+import octoplay from '@/gamedata/slot/octoplay.json'
+import rich88 from '@/gamedata/slot/rich88.json'
+import smartsoft from '@/gamedata/slot/smartsoft.json'
 // import ShowGameItemAMB from './ShowGameItemAMB';
 import axios from 'axios';
 import { getGames } from '@/action/GSC';
@@ -75,28 +75,87 @@ export default function Home() {
         setTotalPage(Math.ceil(pg_soft.length / 24));
 
     }
-    const handleSelectProduct = async (index: number,product_code:number) => {
+    const handleSelectProduct = async (index: number, product_code: number) => {
         localStorage.setItem("slot_active", index.toString());
         setProductActive(index);
-        if(product_code === 1007){
+        if (product_code === 1007) {
             setGames(pg_soft);
             setCurrentGames(pg_soft.slice(0, 24));
             setTotalPage(Math.ceil(pg_soft.length / 24));
-        }else if(product_code === 1050){
+        } else if (product_code === 1050) {
             setGames(playstar);
             setCurrentGames(playstar.slice(0, 24));
             setTotalPage(Math.ceil(playstar.length / 24));
         }
-        else if(product_code === 1006){
+        else if (product_code === 1006) {
             setGames(pragmatic_play);
             setCurrentGames(pragmatic_play.slice(0, 24));
             setTotalPage(Math.ceil(pragmatic_play.length / 24));
+        }
+        else if (product_code === 1079) {
+            setGames(fachai);
+            setCurrentGames(fachai.slice(0, 24));
+            setTotalPage(Math.ceil(fachai.length / 24));
+        }
+        else if (product_code === 1085) {
+            setGames(jdb);
+            setCurrentGames(jdb.slice(0, 24));
+            setTotalPage(Math.ceil(jdb.length / 24));
+        } else if (product_code === 1009) {
+            setGames(cq9);
+            setCurrentGames(cq9.slice(0, 24));
+            setTotalPage(Math.ceil(cq9.length / 24));
+        } else if (product_code === 1018) {
+            setGames(live_22);
+            setCurrentGames(live_22.slice(0, 24));
+            setTotalPage(Math.ceil(live_22.length / 24));
+        } else if (product_code === 1049) {
+            setGames(evoplay);
+            setCurrentGames(evoplay.slice(0, 24));
+            setTotalPage(Math.ceil(evoplay.length / 24));
+        } else if (product_code === 1166) {
+            setGames(no_limit_city);
+            setCurrentGames(no_limit_city.slice(0, 24));
+            setTotalPage(Math.ceil(no_limit_city.length / 24));
+        } else if (product_code === 1168) {
+            setGames(netent);
+            setCurrentGames(netent.slice(0, 24));
+            setTotalPage(Math.ceil(netent.length / 24));
+        } else if (product_code === 1169) {
+            setGames(redtiger);
+            setCurrentGames(redtiger.slice(0, 24));
+            setTotalPage(Math.ceil(redtiger.length / 24));
+        } else if (product_code === 1091) {
+            setGames(jili_tcg);
+            setCurrentGames(jili_tcg.slice(0, 24));
+            setTotalPage(Math.ceil(jili_tcg.length / 24));
+        } else if (product_code === 1115) {
+            setGames(booming_games);
+            setCurrentGames(booming_games.slice(0, 24));
+            setTotalPage(Math.ceil(booming_games.length / 24));
+        } else if (product_code === 1153) {
+            setGames(hacksaw);
+            setCurrentGames(hacksaw.slice(0, 24));
+            setTotalPage(Math.ceil(hacksaw.length / 24));
+        }
+        else if (product_code === 1184) {
+            setGames(rich88);
+            setCurrentGames(rich88.slice(0, 24));
+            setTotalPage(Math.ceil(rich88.length / 24));
+        } else if (product_code === 1170) {
+            setGames(smartsoft);
+            setCurrentGames(smartsoft.slice(0, 24));
+            setTotalPage(Math.ceil(smartsoft.length / 24));
+        } else {
+            setGames(pg_soft);
+            setCurrentGames(pg_soft.slice(0, 24));
+            setTotalPage(Math.ceil(pg_soft.length / 24));
         }
         setPage(0);
     }
 
     const handdleChangPage = async (page: number) => {
-        setCurrentGames(games.slice(24*page, 24*page+24));
+        setCurrentGames(games.slice(24 * page, 24 * page + 24));
         setPage(page);
 
         // const data = await getGames(products[productActive].product_code, "SLOT", page * 24, 24);
@@ -107,8 +166,8 @@ export default function Home() {
     return (
         <>
             <div className='sm:w-full md:w-[960px] lg:w-[1200px] mx-auto'>
-                <div className='flex border rounded-lg '>
-                    <div className='w-[20%] lg:w-[10%] flex flex-col gap-2 bg-gray-800 px-2 py-4'>
+                <div className='flex justify-start border rounded-lg '>
+                    <div className=' w-[20%] md:w-[10%] lg:w-[10%] flex flex-col gap-1 bg-gray-800 px-2 py-4'>
                         {
                             products.map((item, index) => {
                                 return (
@@ -116,7 +175,7 @@ export default function Home() {
                                         onClick={() => {
                                             handleSelectProduct(index, item.product_code);
                                         }}
-                                        className={`bg-white w-full h-[50px] flex items-center border-2  rounded-lg overflow-hidden  ${index == productActive ? 'border-yellow-300' : 'border-purple-600'}`}>
+                                        className={` bg-white w-full h-[50px] flex items-center border-2  rounded-lg overflow-hidden  ${index == productActive ? 'border-yellow-300' : 'border-purple-600'}`}>
                                         <Image
                                             loading='lazy'
                                             src={`/assets/icon/product/${item.product_name}.png`}
@@ -132,8 +191,8 @@ export default function Home() {
                             })
                         }
                     </div>
-                    <div>
-                        <div className=' w-full grid grid-cols-4 md:grid-cols-10 gap-4 p-5 '>
+                    <div className=' w-[80%] md:w-[90%] lg:w-[90%]'>
+                        <div className=' w-full grid grid-cols-4  gap-4 p-5 '>
                             {currentGames && currentGames?.map((item: Game, index) => {
                                 return (
                                     <ShowGameItem
@@ -146,7 +205,7 @@ export default function Home() {
                                 )
                             })}
                         </div>
-                        <div className=' px-2 flex justify-center flex-wrap gap-3'>
+                        <div className='mb-2 px-2 flex justify-center flex-wrap gap-3'>
                             {
                                 Array.from({ length: totalPage }, (_, i) => {
                                     return (
