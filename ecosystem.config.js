bc922d588f16b7d1052ad907ae3be5ba3fd987a5
MonDevInfinity <infinity999.contact@gmail.com>
2025-05-19 08:40:53 +0000

    sss

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+const argEnvIndex = process.argv.indexOf('--env')
+let argEnv = (argEnvIndex !== -1 && process.argv[argEnvIndex + 1]) || ''
+
+const RUN_ENV_MAP = {
+  local: {
+    instances: 2,
+    max_memory_restart: '250M'
+  },
+  dev: {
+    instances: 2,
+    max_memory_restart: '250M'
+  },
+  prod: {
+    instances: "max",
+    max_memory_restart: '15000M'
+  }
+}
+
+if (!(argEnv in RUN_ENV_MAP)) {
+  argEnv = 'prod'
+}
+
+module.exports = {
+  apps: [
+    {
+      name: 'my-app',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      instances: 1,
+      exec_mode: 'cluster',
+      watch: false,
+      max_memory_restart: '12000M',
+      env_local: {
+        APP_ENV: 'local'
+      },
+      env_dev: {
+        APP_ENV: 'dev'
+      },
+      env_prod: {
+        APP_ENV: 'prod'
+      }
+    }
+  ]
+}
